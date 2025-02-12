9a6bd734a366b52a0505179f4ed6e3d33a19f26c
Jacob Aust <jaust.413@gmail.com>
2025-02-11 17:48:51 -0800

    Downloaded and Setup Local Fonts

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,12 @@
 export default function Home() {
     return (
-        <div className="text-3xl font-black text-primary-500">
-            Welcome to DevFlow
-        </div>
+        <>
+            <h1 className="text-3xl font-black text-primary-500">
+                Welcome to DevFlow
+            </h1>
+            <h1 className="text-3xl font-black text-primary-500 font-space-grotesk">
+                Welcome to DevFlow (Space Grotesk)
+            </h1>
+        </>
     );
 }
