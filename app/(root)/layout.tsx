bd93b8845327a2d77fa2ff662523d1925b5f626b
Jacob Aust <jaust.413@gmail.com>
2025-02-25 15:24:39 -0800

    Setup Auth Layouts, Social Auth Form, Next Auth, OAuth with Google and GitHub Providers

---
+++ app/(root)/layout.tsx
@@ -0,0 +1,13 @@
+import Navbar from "@/components/navigation/navbar";
+import ReactNode from "react";
+
+const RootLayout = ({ children }: { children: React.ReactNode }) => {
+    return (
+        <main>
+            <Navbar />
+            {children}
+        </main>
+    );
+};
+
+export default RootLayout;
